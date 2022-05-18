0ed70b9bd983c8a51c0629aff93b8d36be98e558
Pindelta <48892727+Pindelta@users.noreply.github.com>
2022-05-18 18:25:45 -0500

    Update FlairTrailsAndSpinner.js

--- FlairTrailsAndSpinner.js
+++ FlairTrailsAndSpinner.js
@@ -1,28 +1,23 @@
 // ==UserScript==
-// @name            TagPro Flair Trails and Spinner
-// @version         0.2.2
+// @name            TagPro Flair Trails and Spin
+// @version         0.3.0
 // @description     Have sick trails for all your flairs just like the arc reactor. (Without having to level up) But wait there's more. You can also have your flairs spin like the level 4 donor flair absolutely free.
 // @match           *://*.koalabeast.com/*
 // @author          Pindelta
 // @supportURL      https://www.reddit.com/message/compose/?to=Pindelta
-// @require      https://greasyfork.org/scripts/371240-TPUL.js
-// @namespace       https://github.com/Pindelta/TagPro-Flair-Trails-and-Spinner
+// @require         https://greasyfork.org/scripts/371240-TPUL.js
+// @namespace       https://github.com/Pindelta/TagPro-Flair-Trails-and-Spin
 
 // ==/UserScript==
 
+// - v0.3.0: added settings menu in the in-game settings
 // - v0.2.2: fixed arc reactor flair not showing it's original trail, you don't need to put your display name anymore
 // - v0.2.1: changed included domains to *://*.koalabeast.com/*, which should encompass all koalabeast url variations
 // - v0.2.0: added https://koalabeast.com* to list of domains, added option to apply spin effect to all balls
 
 // ========= SETTINGS =========
-// Set either of these to true to enable the respective effect.
-// It looks wonky with both on, so set one to true and the other to false.
-// Feel free to do turn em both on tho, it won't break anything(I think)
-const spinFlair = true;
-const useTrail = false;
+// All settings can be configured from the settings in TagPro!
 
-// set this to true to add spin effect to everyone's ball
-const addSpinToAllBalls = false;
 // ========= END OF SETTINGS =========
 
 const customTrailDefinition = {
@@ -48,7 +43,67 @@ const customTrailDefinition = {
 /* global tagpro, tagproConfig, $, tpul */
 
 tagpro.ready(function () {
+  if (tpul.playerLocation == "profile") {
+    $("#settings, .card:first").before(
+      '<div id=flair-trail-and-spin-select class="profile-settings block"><h3 class=header-title>Flair Trail and Spin</h3><form class=form-horizontal><div class=form-group></div><hr><div id=save-group class=form-group>'
+    );
+
+    $("#flair-trail-and-spin-select .form-group:first").before(
+      '<label class="col-sm-4 control-label">Effects</label><div class="col-sm-8" style="margin-bottom: 15px"><div class="checkbox"><label for="spin"><input id="spin" name="spin" type="checkbox" checked="">Enable Flair Spin</label></div><div class="checkbox"><label for="trail"><input id="trail" name="trail" type="checkbox" checked="">Enable Flair Trail</label></div></div><label class="col-sm-4 control-label">Apply Effects To All Players</label><div class="col-sm-8"><div class="checkbox"><label for="spinToAll"><input id="spinToAll" name="spinToAll" type="checkbox" checked="">Flair Spin</label></div></div>'
+    );
+
+    $("#flair-trail-and-spin-select")
+      .find("input")
+      .each(function () {
+        var name = $(this).prop("name");
+        $(this).prop("checked", $.cookie(name) == "true");
+      })
+      .prop("disabled", false);
+
+    $("#save-group").append(
+      '<div class="col-sm-12 text-right"><div id=script-status style="display: none;">Settings saved!</div><button id="scriptScriptSettings" class="btn" type="button">Save Script Settings'
+    );
+    $("#script-status").css("margin-bottom", "20px");
+
+    $("#save-group button").click(function () {
+      // Get the current selection
+
+      var settings = $("#settings .js-cookie:checked")
+        .map(function (i, setting) {
+          $(setting).data("setting");
+        })
+        .get();
+
+      // Save to the cookies
+
+      $("#flair-trail-and-spin-select")
+        .find("input")
+        .each(function () {
+          var name = $(this).prop("name"),
+            checked = $(this).prop("checked");
+          $.cookie(name, checked, {
+            expires: 36500,
+            path: "/",
+            domain: tagproConfig.cookieHost,
+          });
+        });
+
+      // Show some feedback
+
+      $("#script-status").slideDown();
+      setTimeout(function () {
+        $("#script-status").slideUp();
+      }, 3e3);
+    });
+  }
+
   if (tpul.playerLocation === "game") {
+    const spinFlair = $.cookie("spin") ? $.cookie("spin") : false;
+    const useTrail = $.cookie("trail") ? $.cookie("trail") : false;
+    const addSpinToAllBalls = $.cookie("spinToAll")
+      ? $.cookie("spinToAll")
+      : false;
+
     // Override the drawFlair function
     tagpro.renderer.drawFlair = function (player) {
       const isYourBall = player.id === tagpro.playerId; // check if this is your ball
